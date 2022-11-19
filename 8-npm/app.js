a1f4a644f34ede03e778c38ad7db0bae0e7fde2a
kirafadhil <95693500n@gmail.com>
2022-11-19 13:52:14 +0700

    learn npm: validator,chalk

---
+++ 8-npm/app.js
@@ -0,0 +1,11 @@
+// const validator = require('validator');
+import chalk from 'chalk';
+
+// console.log(validator.isEmail('fadhil@gmail.c'));
+// console.log(validator.isMobilePhone('082234675','id-ID'));
+// console.log(validator.isNumeric('0822346s75'));
+// console.log(chalk.italic.black.bgBlue('Hello World'));
+
+const pesan = chalk`lorem, {bgBlue.Red yahahah} hayyyu`;
+console.log(pesan);
+

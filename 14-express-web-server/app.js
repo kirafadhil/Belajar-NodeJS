6f70afaa8e2e35739826e62bc5d3bb024b4fd290
kirafadhil <95693500n@gmail.com>
2022-11-20 22:29:07 +0700

    learn express method sendFile

--- 14-express-web-server/app.js
+++ 14-express-web-server/app.js
@@ -3,22 +3,31 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile('./index.html', {root: __dirname});
 });
 
 app.get("/about", (req, res) => {
-    res.send("Ini adalah halaman about");
-  });
+  res.sendFile('./about.html', {root: __dirname});
+});
 
-  app.get("/contact", (req, res) => {
-    res.send("Ini adalah halaman contact");
-  });
+app.get("/contact", (req, res) => {
+  res.sendFile('./contact.html', {root: __dirname});
+});
+
+app.use('/', (req, res) => {
+  res.status(404);
+  res.send("404");
+  // res.json({
+  //   nama: 'Muhammad Naufal',
+  //   email: 'kira@gmail.com',
+  //   noHP: '0812434354',
+  // });
+  // res.sendFile('./index.html', {root: __dirname});
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
 
 // // const express = require('express')
 // const http = require("http")

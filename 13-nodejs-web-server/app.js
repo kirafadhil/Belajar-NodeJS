43d880dfd7511ba6a0c51dce232e383224c94dac
kirafadhil <95693500n@gmail.com>
2022-11-20 13:59:19 +0700

    create simple nodejs web server

---
+++ 13-nodejs-web-server/app.js
@@ -0,0 +1,43 @@
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+
+const renderHTML = (path, res) => {
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write("File not found");
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+};
+
+const server = http
+  .createServer((req, res) => {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+
+    const url = req.url;
+    if (url === "/about") {
+      renderHTML("./about.html", res);
+    } else if (url === "/contact") {
+      renderHTML("./contact.html", res);
+    } else {
+      renderHTML("./index.html", res);
+      //   fs.readFile('./index.html', (err, data) =>{
+      //     if(err){
+      //         res.writeHead(404);
+      //         res.write('File not found');
+      //     } else {
+      //         res.write(data);
+      //     }
+      //     res.end();
+      //   });
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server is listening on port ${port}..`);
+  });

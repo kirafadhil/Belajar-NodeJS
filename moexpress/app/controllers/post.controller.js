5d85968c1890a4bd74c1c61468784716d757d2c8
kirafadhil <95693500n@gmail.com>
2022-11-22 10:48:11 +0700

    add delete data by id

--- moexpress/app/controllers/post.controller.js
+++ moexpress/app/controllers/post.controller.js
@@ -48,21 +48,43 @@ exports.findOne = (req, res) => {
 };
 
 exports.update = (req, res) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    Post.findByIdAndUpdate(id, req.body)
+  Post.findByIdAndUpdate(id, req.body)
     .then((result) => {
-        if(!result){
-            res.status(404).send({
-                message: "Post not found"
-            })
-        }
-        res.send({
-            message: "Post was updated"
-        })
-    }).catch((err) => {
-        res.status(500).send({
-            message: err.message || "Something error while update post.",
-          });
+      if (!result) {
+        res.status(404).send({
+          message: "Post not found",
+        });
+      }
+      res.send({
+        message: "Post was updated",
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Something error while update post.",
+      });
     });
-}
+};
+
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Post.findByIdAndRemove(id)
+    .then((result) => {
+      if (!result) {
+        res.status(404).send({
+          message: "Post not found",
+        });
+      }
+      res.send({
+        message: "Post was deleted",
+      });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Something error while delete post.",
+      });
+    });
+};

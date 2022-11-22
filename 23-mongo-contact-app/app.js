1d23153c8195026ee334cc5944af86121f6f0716
kirafadhil <95693500n@gmail.com>
2022-11-22 19:55:52 +0700

    add flash msg, page contact, detail contact

--- 23-mongo-contact-app/app.js
+++ 23-mongo-contact-app/app.js
@@ -1,14 +1,36 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const { db } = require("./model/contact");
+
+// connect db.js
+require("./utils/db");
+const Contact = require("./model/contact");
+
+// setup flash
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
 
 const app = express();
 const port = 3000;
 
 // Setup EJS
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+
+// konfigurasi flash
+app.use(cookieParser("secret"));
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
 
 // halaman home
 app.get("/", (req, res) => {
@@ -36,21 +58,38 @@ app.get("/", (req, res) => {
 
 // halaman about
 app.get("/about", (req, res) => {
-    res.render("about", {
-      title: "Halaman About",
-      layout: "layouts/main-layout",
-    });
+  res.render("about", {
+    title: "Halaman About",
+    layout: "layouts/main-layout",
   });
+});
 
 // halaman contact
-app.get("/contact", (req, res) => {
-    const contacts = loadContact();
-  
-    res.render("contact", {
-      title: "Halaman Contact",
+app.get("/contact", async (req, res) => {
+  // Contact.find().then((contact) => {
+  //     res.send(contact)
+  // }).catch((err) => {
+  //     res.send(err)
+  // });
+
+  const contacts = await Contact.find();
+
+  res.render("contact", {
+    title: "Halaman Contact",
+    layout: "layouts/main-layout",
+    contacts,
+    msg: req.flash("msg"),
+  });
+});
+
+// halaman detail kontak
+app.get("/contact/:nama", async (req, res) => {
+    const contact = await Contact.findOne({nama: req.params.nama});
+    
+    res.render("detail", {
+      title: "Halaman Detail Contact",
       layout: "layouts/main-layout",
-      contacts,
-      msg: req.flash("msg"),
+      contact,
     });
   });
 
